8c4e6439853999d205fa89b599a76a5f7114dc5e
HeshamGamil <hshamj343@gmail.com>
2024-08-26 14:35:49 +0300

    uploading lab1 for j

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
-// Write your code here
+var num1=31;
+var num2=2;
+var multiply=num1*num2;
+//------------------------------------------------------------------
+var random=Math.ceil(Math.random()*1)
+//--------------------------------------------------
+var num3=4;
+var num4=5;
+var mod=num3%num4;
+//--------------------------
+let max=Math.max(1,2,3,10,20)
